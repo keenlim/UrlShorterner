93723f03154ca6c1b1e83e295ac4f18cf4f44cbd
keen <kelim1221@gmail.com>
2023-03-08 01:17:16 +0800

    validateURLhelperfunction

---
+++ backend/HelperFunctions/util.js
@@ -0,0 +1,25 @@
+// Function to validate URL - To ensure that any URL passed follows HTTP protocol 
+
+//Function to validate URL using regex 
+function validateUrl(value){
+
+    var urlPattern = new RegExp('^(https?:\\/\\/)?'+ // validate protocol
+	    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // validate domain name
+	    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // validate OR ip (v4) address
+	    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // validate port and path
+	    '(\\?[;&a-z\\d%_.~+=-]*)?'+ // validate query string
+	    '(\\#[-a-z\\d_]*)?$','i');
+
+      return urlPattern.test(value);
+
+}
+
+//Function to validate valid HTTP URL using URL Constructor
+function isValidUrl(string){
+    try{
+        const newUrl = new URL(string);
+        return newUrl.protocol === 'http' || newUrl.protocol === 'https';
+    }catch(err){
+        return false;
+    }
+}
